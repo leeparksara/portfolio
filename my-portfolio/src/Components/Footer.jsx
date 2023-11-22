61ff05f406d712d72c1c187fb2220ab3dce977bd
leeparksara <90184658+leeparksara@users.noreply.github.com>
2023-11-22 18:08:34 +0900

    .

--- my-portfolio/src/Components/Footer.jsx
+++ my-portfolio/src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import { SocialIcon } from 'react-social-icons'
 const Footer = () => {
   return (
     <div className='footer'>
-        <p>sara lee</p>
+        <p>© Sara Leepark 2023</p>
         <div className='footer-links'>
         <SocialIcon href='https://github.com/leeparksara' url="www.github.com" />
         <SocialIcon  href='https://www.linkedin.com/in/sara-leepark-5ba013263/' url="www.linkedin.com" /> 
